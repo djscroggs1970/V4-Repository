2997b78dcd068ad1fbb01b92986512450bc2b4c0
djscroggs1970 <davidscroggs49@gmail.com>
2026-04-24 18:01:44 -0400

    Add domain index

---
+++ packages/domain/src/index.ts
@@ -0,0 +1,3 @@
+export { DATA_LAYERS, SOURCE_ORIGINS, REVIEW_STATUSES, TAKEOFF_MODES, DEPTH_CLASSES } from "./enums.js";
+export type { DataLayer, SourceOrigin, ReviewStatus, TakeoffMode, DepthClass } from "./enums.js";
+export type { BoundaryFields, ProjectInstance, SourceDocument, DrawingSheet, TakeoffItem, ProductionRateRule, EstimateLine } from "./models.js";
