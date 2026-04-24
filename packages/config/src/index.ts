708f7ee4a981fe89f31d6c9b7ec63819311ec7b1
djscroggs1970 <davidscroggs49@gmail.com>
2026-04-24 18:02:49 -0400

    Add config constants

---
+++ packages/config/src/index.ts
@@ -0,0 +1,9 @@
+export const FRAMEWORK_VERSION = "v0.1-bootstrap";
+export const PHASE_1_WORK_TYPE = "pvc_gravity_sewer";
+export const DEFAULT_REGION = "region_01";
+
+export const STORAGE_LAYERS = {
+  framework: "00_Framework",
+  projectInstances: "10_Project_Instances",
+  sandbox: "20_Test_Sandbox"
+} as const;
