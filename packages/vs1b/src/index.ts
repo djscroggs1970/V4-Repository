238eb842b8c7c7694adfa655cb6f19cf281e24be
djscroggs1970 <davidscroggs49@gmail.com>
2026-04-24 23:45:03 -0400

    Export VS1B cost scenario output manifest

--- packages/vs1b/src/index.ts
+++ packages/vs1b/src/index.ts
@@ -3,6 +3,8 @@ import type { DepthClass } from "@v4/domain";
 import type { QuantityExportObject, QuantitySummaryLine } from "@v4/vs1a";
 export { buildCostInputRegistry } from "./cost-input-registry.js";
 export type { CostInputRegistry, CostInputRegistryInput } from "./cost-input-registry.js";
+export { buildCostScenarioOutputManifest } from "./cost-scenario-output.js";
+export type { CostScenarioOutputInput, CostScenarioOutputManifest } from "./cost-scenario-output.js";
 
 export interface MaterialQuoteInput {
   quote_id: string;
